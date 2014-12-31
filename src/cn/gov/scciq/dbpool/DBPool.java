f5e582ff17b6053f2accf23385a4ba6300b7a5f8
chao.xu <chao.xu@l-001806.envisioncn.com>
2014-12-31 21:01:09 +0800

    系统接单功能

--- src/cn/gov/scciq/dbpool/DBPool.java
+++ src/cn/gov/scciq/dbpool/DBPool.java
@@ -6,12 +6,15 @@ import javax.naming.NamingException;
 
 public class DBPool {
 	public static javax.sql.DataSource ds;
+	public static javax.sql.DataSource ds_cems;
 	static {
 		try {
 			Context initCtx = new InitialContext();
 			Context ctx = (Context) initCtx.lookup("java:comp/env");
 			Object obj = (Object) ctx.lookup("jdbc/FRM_SC");
 			ds = (javax.sql.DataSource) obj;
+			Object obj2 = (Object) ctx.lookup("jdbc/CEMS");
+			ds_cems = (javax.sql.DataSource) obj2;
 		} catch (NamingException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
